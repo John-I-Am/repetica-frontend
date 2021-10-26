237ef8af4e70e2a191b42ef671a40ba9e4ce56f0
John He <47208192+John-I-Am@users.noreply.github.com>
2021-10-26 17:53:12 +1300

    Implement service for dictionary lookup

---
+++ src/services/dictionary.tsx
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const baseUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+
+const getDefinition = async (word: string) => {
+  const response = await axios.get(`${baseUrl}${word}`);
+  return response.data;
+};
+
+export default { getDefinition };
