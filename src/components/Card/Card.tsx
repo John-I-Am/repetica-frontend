e7aeadcb635637d4c56ab1032aee1cc2add57eb8
John He <47208192+John-I-Am@users.noreply.github.com>
2021-10-29 15:50:03 +1300

    Implement adding and loading of new cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,55 +1,54 @@
-/* eslint-disable react/button-has-type */
 /* eslint-disable jsx-a11y/media-has-caption */
-/* eslint-disable space-in-parens */
-/* eslint-disable no-unused-vars */
 import './style.css';
 import { useSelector } from 'react-redux';
-import { useState, useEffect } from 'react';
-import dictionaryService from '../../services/dictionary';
+import { useState } from 'react';
 
 const Card = () => {
   const cards = useSelector((state: any) => state.card);
   const [currentCard, setCurrentCard] = useState(0);
-  const [front, setFront] = useState('null');
-  const [back, setBack] = useState('null');
-  const [audio, setAudio] = useState('');
 
-  useEffect(() => {
-    const getDefine = async () => {
-      try {
-        setFront(cards[currentCard].front);
+  const handleGetNext = async () => {
+    if (currentCard === cards.length - 1) {
+      console.log('outta bound');
+    } else {
+      setCurrentCard(currentCard + 1);
+    }
+  };
 
-        const define: any = await dictionaryService.getDefinition(front);
-        setBack(define[0].meanings[0].definitions[0].definition);
+  // const getAudio = () => {
+  //   const temp = JSON.parse(cards[currentCard].back);
+  //   console.log(temp.phonetics[0].audio);
+  //   const temp2 = (temp.phonetics[0].audio);
+  //   return temp2;
+  // };
 
-        const mediaElem: any = document.getElementById('audio');
-        mediaElem.load();
-        setAudio(define[0].phonetics[0].audio);
-      } catch {
-        alert('no more cards');
-      }
-    };
-    getDefine();
-  });
-
-  const handleSearch = async () => {
-    setCurrentCard(currentCard + 1);
+  const play = () => {
+    const audio: any = document.getElementById('audio');
+    audio.play();
   };
 
   return (
     <div id="card">
+      <div id="card-header">
+        {`Level ${cards[currentCard].level}`}
+        <svg onClick={play} width="24" height="24" fill="grey" viewBox="0 0 24 24">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M18.25 12L5.75 5.75V18.25L18.25 12Z" />
+        </svg>
+        <audio id="audio" src={`//ssl.gstatic.com/dictionary/static/sounds/20200429/${cards[currentCard].front}--_gb_1.mp3`} />
+      </div>
+
       <div id="front">
-        {front}
-        <audio id="audio" controls>
-          <source src={audio} type="audio/mp3" />
-        </audio>
+        {cards[currentCard].front}
       </div>
       <div id="back">
-        {back}
+        {JSON.parse(cards[currentCard].back).map((ele: any) => (
+          <div id="card-definition">
+            {ele.definitions[0].definition}
+          </div>
+        ))}
       </div>
-      <button onClick={handleSearch}>
-        this is button
-      </button>
+
+      <button type="button" onClick={handleGetNext}>Next Card</button>
     </div>
   );
 };
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,52 @@
 import './style.css';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import dictionaryService from '../../services/dictionary';
+import { createCard } from '../../reducers/cardReducer';
 import Card from '../../components/Card/Card';
 import NavBar from '../../components/NavBar/NavBar';
+import cardless from '../../assets/cardless.png';
 
-const HomePage = () => (
-  <div id="homePage">
-    <Card />
-    <NavBar />
-  </div>
-);
+const HomePage = () => {
+  const cards = useSelector((state: any) => state.card);
+  const [text, setText] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const response: any = await dictionaryService.getDefinition(text);
+    const stringifiedResponse = JSON.stringify(response[0].meanings);
+
+    const card = {
+      front: text,
+      back: stringifiedResponse,
+      level: 1,
+    };
+
+    dispatch(createCard(card));
+  };
+  const noCards = () => {
+    console.log('');
+    return (
+      <div>
+        <p>NO CARDS FOUND, DOG ATE YOUR HOMEWORK?</p>
+        <img height="600px" src={cardless} alt="idk" />
+      </div>
+
+    );
+  };
+
+  return (
+    <div id="homePage">
+      {cards.length !== 0 ? <Card /> : noCards()}
+      <form onSubmit={handleSubmit}>
+        <input onChange={({ target }) => setText(target.value)} />
+      </form>
+
+      <NavBar />
+    </div>
+  );
+};
 
 export default HomePage;
