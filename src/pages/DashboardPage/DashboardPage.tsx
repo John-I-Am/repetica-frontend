6562e8983ae833901c62f9dfba99238543af04cf
John He <47208192+John-I-Am@users.noreply.github.com>
2021-10-31 20:45:39 +1300

    Improve empty cards screen

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -5,8 +5,28 @@ import { useSelector, useDispatch } from 'react-redux';
 import { initializeCards } from '../../reducers/cardReducer';
 import Card from '../../components/Card/Card';
 import NavBar from '../../components/NavBar/NavBar';
-import cardless from '../../assets/cardless.png';
+import cardless from '../../assets/cardless.svg';
 import { setFromLocal } from '../../reducers/userReducer';
+
+interface CardlessProps {
+  totalCards: number;
+  activeCards: number;
+}
+
+const Cardless = ({ totalCards, activeCards }: CardlessProps) => (
+  <div className="cardless">
+    <img src={cardless} alt="empty deck" />
+    <h1>No Cards ;()</h1>
+    <div>
+      <h2>
+        <b>{activeCards}</b>
+        {' Cards Ready For Review Out Of '}
+        <b>{totalCards}</b>
+        {' Cards.'}
+      </h2>
+    </div>
+  </div>
+);
 
 const DashboardPage = () => {
   const cards = useSelector((state: any) => state.card);
@@ -23,15 +43,9 @@ const DashboardPage = () => {
     }
   }, []);
 
-  const noCards = () => (
-    <div>
-      <img height="600px" src={cardless} alt="empty deck" />
-    </div>
-  );
-
   return (
     <div id="dashboardPage">
-      {cardsToStudy.length !== 0 ? <Card /> : noCards()}
+      {cardsToStudy.length !== 0 ? <Card /> : <Cardless totalCards={cards.length} activeCards={cardsToStudy.length} />}
       <NavBar />
     </div>
   );
