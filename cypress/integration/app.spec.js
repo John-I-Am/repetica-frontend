fa8a3c2a2ad347e32f082c300ca1547df3befc06
John He <47208192+John-I-Am@users.noreply.github.com>
2021-11-03 13:42:18 +1300

    Implement cypress end-to-end testing

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,52 @@
+describe('App test', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      email: "root@root",
+      name: "root",
+      surname: "root",
+      password: "rootroot"
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:3000');
+  });
+
+  it('front page be can opened', function() {
+    cy.contains('Work in Progress');
+  });
+
+  it('login form can be opened', function() {
+    cy.contains('Login').click();
+  });
+
+  it('user can login', function() {
+    cy.contains('Login').click();
+    cy.get('#email').type('root@root');
+    cy.get('#password').type('rootroot');
+    cy.get('#login-button').click();
+    cy.contains('No Cards');
+  });
+
+  it('login fails with wrong credentials', function() {
+    cy.contains('Login').click();
+    cy.get('#email').type('root@root');
+    cy.get('#password').type('root');
+    cy.get('#login-button').click();
+
+    cy.get('#error')
+      .contains('Incorrect Credentials');
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.login({ username: 'root@root', password: 'rootroot' })
+    })
+
+    it('a new note can be created', function() {
+      cy.get('#deck-nav').click()
+      cy.get('input').type('test')
+      cy.get('form').submit()
+      cy.contains('Now')
+    })
+  })
+});
