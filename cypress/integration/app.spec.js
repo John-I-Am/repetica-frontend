49d42c7a115d92367cb24dd04e512fff4c648e49
John He <47208192+John-I-Am@users.noreply.github.com>
2021-11-06 15:23:01 +1300

    Fix eslint

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,25 +1,26 @@
-describe('App test', function() {
-  beforeEach(function() {
-    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+/* eslint-disable prefer-arrow-callback */
+describe('App test', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset');
     const user = {
-      email: "root@root",
-      name: "root",
-      surname: "root",
-      password: "rootroot"
-    }
-    cy.request('POST', 'http://localhost:3001/api/users/', user)
+      email: 'root@root',
+      name: 'root',
+      surname: 'root',
+      password: 'rootroot',
+    };
+    cy.request('POST', 'http://localhost:3001/api/users/', user);
     cy.visit('http://localhost:3000');
   });
 
-  it('front page be can opened', function() {
+  it('front page be can opened', function () {
     cy.contains('Work in Progress');
   });
 
-  it('login form can be opened', function() {
+  it('login form can be opened', function () {
     cy.contains('Login').click();
   });
 
-  it('user can login', function() {
+  it('user can login', function () {
     cy.contains('Login').click();
     cy.get('#email').type('root@root');
     cy.get('#password').type('rootroot');
@@ -27,7 +28,7 @@ describe('App test', function() {
     cy.contains('No Cards');
   });
 
-  it('login fails with wrong credentials', function() {
+  it('login fails with wrong credentials', function () {
     cy.contains('Login').click();
     cy.get('#email').type('root@root');
     cy.get('#password').type('root');
@@ -35,18 +36,18 @@ describe('App test', function() {
 
     cy.get('#error')
       .contains('Incorrect Credentials');
-  })
+  });
 
-  describe('when logged in', function() {
-    beforeEach(function() {
-      cy.login({ username: 'root@root', password: 'rootroot' })
-    })
+  describe('when logged in', function () {
+    beforeEach(function () {
+      cy.login({ username: 'root@root', password: 'rootroot' });
+    });
 
-    it('a new note can be created', function() {
-      cy.get('#deck-nav').click()
-      cy.get('input').type('test')
-      cy.get('form').submit()
-      cy.contains('Now')
-    })
-  })
+    it('a new note can be created', function () {
+      cy.get('#deck-nav').click();
+      cy.get('input').type('test');
+      cy.get('form').submit();
+      cy.contains('Now');
+    });
+  });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,14 +24,14 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('login', ({ email, password}) => {
+Cypress.Commands.add('login', ({ email, password }) => {
   cy.request('POST', 'http://localhost:3001/api/login', {
-    email, password
-  }). then(({ body }) => {
-    localStorage.setItem('currentUser', JSON.stringify(body))
-    cy.visit('http://localhost:3000')
-  })
-})
+    email, password,
+  }).then(({ body }) => {
+    localStorage.setItem('currentUser', JSON.stringify(body));
+    cy.visit('http://localhost:3000');
+  });
+});
 
 Cypress.Commands.add('createCard', ({ front, back, level }) => {
   cy.request({
@@ -39,8 +39,8 @@ Cypress.Commands.add('createCard', ({ front, back, level }) => {
     method: postMessage,
     body: { front, back, level },
     headers: {
-      'Authorization': `bearer ${JSON.parse(localStorage.getItem('currentUser')).token}`
-    }
-  })
-  cy.visit('http://localhost:3000')
-})
+      Authorization: `bearer ${JSON.parse(localStorage.getItem('currentUser')).token}`,
+    },
+  });
+  cy.visit('http://localhost:3000');
+});
